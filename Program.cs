6f995922c47a9c8df036f954c8d7442ec4b3bb97
kalakal <sayedfaizanqadri@gmail.com>
2022-11-28 13:45:22 +0530

    regex problem

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace UserRegistrationProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration Problems Statement Using REGEX");
+            UserValidation userValidation = new UserValidation();
+            Console.WriteLine("Enter The First Name: ");
+            string firstname = Console.ReadLine();
+            userValidation.ValidationFirstName(firstname);
+        }
+    }
+}
