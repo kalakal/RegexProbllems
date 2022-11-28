a1d7f76a6461296bd35175e0bff1977d394bdb39
kalakal <sayedfaizanqadri@gmail.com>
2022-11-28 14:03:45 +0530

    emailvalidation problem added

--- Program.cs
+++ Program.cs
@@ -11,6 +11,15 @@ namespace UserRegistrationProblem
             Console.WriteLine("Enter The First Name: ");
             string firstname = Console.ReadLine();
             userValidation.ValidationFirstName(firstname);
+
+            Console.WriteLine("Enter The Last Name: ");
+            string lastname = Console.ReadLine();
+            userValidation.ValidationLastName(lastname);
+
+            Console.WriteLine("Enter Email: ");
+            string email = Console.ReadLine();
+            userValidation.ValidationFirstName(email);
+
         }
     }
 }
